f891240d13e8f01f9fc7217a3e49d82ad5af1a1e
SuperNoice <SuperNoice@yandex.ru>
2022-06-22 18:08:57 +0300

    Add AuthorizationController

---
+++ Test/Controllers/AuthorizationController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Test.Services;
+
+namespace Test.Controllers
+{
+    [Route("authorization")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly ILogger<AuthorizationController> _logger;
+        private readonly DatabaseController _db;
+
+        public AuthorizationController(ILogger<AuthorizationController> logger, DatabaseController db)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public async Task<IActionResult> Login(string userId)
+        {
+
+            if (!Guid.TryParse(userId, out var userGuid))
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var user = await _db.GetUserByGuid(new Guid(userId));
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                await _db.SignInUser(user);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Cannot process login: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("registration")]
+        public async Task<IActionResult> Registration()
+        {
+            try
+            {
+                return Ok(await _db.CreateUser());
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Cannot process registration: ");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
