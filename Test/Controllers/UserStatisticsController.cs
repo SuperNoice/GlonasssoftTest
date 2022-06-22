da9b23e2902e269e7d603a4d81640dde35a62abe
SuperNoice <SuperNoice@yandex.ru>
2022-06-22 12:40:12 +0300

    Добавьте файлы проекта.

---
+++ Test/Controllers/UserStatisticsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Route("report")]
+    public class UserStatisticsController : ControllerBase
+    {
+        private readonly ILogger<UserStatisticsController> _logger;
+
+        public UserStatisticsController(ILogger<UserStatisticsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("info")]
+        public void Get()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("user_statistic")]
+        public void Post()
+        {
+
+        }
+    }
+}
