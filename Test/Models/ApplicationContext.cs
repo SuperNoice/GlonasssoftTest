80193f803b8695c514462eba29bd782b4226eb96
SuperNoice <SuperNoice@yandex.ru>
2022-06-22 14:23:58 +0300

    Add database

---
+++ Test/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SignIn> SignIns { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Test/Startup.cs
+++ Test/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Test.Models;
 
 namespace Test
 {
@@ -25,6 +27,9 @@ namespace Test
         public void ConfigureServices(IServiceCollection services)
         {
             var minProcessingTimeMilliseconds = Configuration.GetSection("MinProcessingTimeMiliseconds").Get<string>();
+            var dbConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(dbConnectionString));
 
             services.AddSingleton<string>(minProcessingTimeMilliseconds);
             services.AddControllers();
